b91cbdcad0bf09579adeacf7c34f11e4f7f5da34
Clevison <cleversonmaiden031@gmail.com>
2025-08-02 23:33:42 -0500

    mudança

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
-let num = document.getElementById('fnum') //pega o número
-let lista = document.getElementById('flista') //pega o número e add na lista
-let res = document.getElementById('res') // resultado
-let valores = [] //array vazio para add um número
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
 
-function adicionar(){ //função clicar na caixa
-   if(isNum(num.value) && !inLista(num.value, valores)){
-    valores.push(Number(num.value))
-    let item = document.createElement('option')
-    item.text = `O ${num.value} foi adicionado`
-    lista.appendChild(item)
-    res.innerHTML= ''
-   } else{
-     window.alert('Adicione um número informado')
-   }
-   num.value = ''
-   num.focus()
+function adicionar(){
+    if(numero(num.value) && !isLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `O ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+
+    }else{
+        window.alert('Informe o número informado')
+    }
+    num.value=''
+    num.focus()
 }
-
-function isNum(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
+function numero(n){
+    if(Number(n) >= 1  && Number(n) <= 100){
         return true
-
     } else{
         return false
     }
 }
-
-function inLista(n, l){
-    if(l.indexOf(Number(n)) != -1){
+function isLista(n, listar){
+    if(listar.indexOf(n) != -1){
         return true
-    }else{
+    } else{
         return false
     }
-}
 
+}
 function enter(){
     if(valores.length == 0){
-        window.alert('Adicione um número')
-    }else{
+        window.alert('Adicione um valor')
+    } else{
         let total = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
         let soma = 0
+        let maior = [0]
+        let menor = [0]
         let media = 0
         for(let pos in valores){
             soma += valores[pos]
 
             if(valores[pos] > maior)
                 maior = valores[pos]
-            if (valores[pos] < menor)
+
+            if(valores[pos] < menor)
                 menor = valores[pos]
         }
         media = soma / total
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao total temos ${total} números cadastrados aqui!</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${maior} </p>`
-        res.innerHTML += `<p>O menor valor informado foi ${menor} </p>`
-        res.innerHTML += `<p>A soma está entre o valor são ${soma}</p>`
-        res.innerHTML += `<p>A média dos valores são de ${media}</p>`
+        res.innerHTML += `O total é ${total}`
+        res.innerHTML +=`<p>A soma é ${soma}</p>`
+        res.innerHTML += ` <p>O maior número é o ${maior}</p>`
+        res.innerHTML += `<p>O menor número é o ${menor}</p>`
+        res.innerHTML += `<p>A média é ${media}</p>`
+        
+
+
     }
+
 }
